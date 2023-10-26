07257c9ba3d23e27291731fb35859f6035c4424e
Levente Major <leventemajor36@gmail.com>
2023-10-26 09:57:17 +0200

    Add project files.

---
+++ Nepesseg3/Orszag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nepesseg3
+{
+    internal class Orszag {
+
+    public string Orszagnev { get; private set; }
+    public int Terulet { get; private set; }
+    public int Nepesseg { get; private set; }
+    public string Fovaros { get; private set; }
+    public int FovarosNepesseg { get; private set; }
+
+    public Orszag(string sor)
+    {
+        var darabok = sor.Split(';');
+        Orszagnev = darabok[0];
+        Terulet = int.Parse(darabok[1]);
+        Nepesseg = int.Parse(darabok[2].Replace("g","0000"));
+        Fovaros = darabok[3];
+        FovarosNepesseg = int.Parse(darabok[4]);
+    }
+
+    public override string ToString()
+    {
+        return "";
+    }
+
+    }
+}
---
+++ Nepesseg3/Program.cs
@@ -0,0 +1,12 @@
+﻿using Nepesseg3;
+using System.Text;
+
+var orszagok = new List<Orszag>();
+using StreamReader sr = new(@"..\..\..\src\adatok-utf8.txt", Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    orszagok.Add(new Orszag(sr.ReadLine()));
+}
+
+Console.WriteLine(orszagok.Count);
